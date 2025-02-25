bc7ae5e9cc36cccd0efc3fa3c5386be5c5740489
Uniko2398 <Uniko2301@gmail.com>
2025-02-25 16:14:00 +0100

    Add files via upload

---
+++ esercizio2.sql
@@ -0,0 +1,11 @@
+select * from Products
+
+select * from Products where (UnitsInStock > 40)
+ 
+select * from Employees where (City = 'London')
+
+select * from Orders order by Freight
+
+select * from Orders where (Freight > 90) and (Freight < 200)
+
+select * from Products where CategoryID = '1'
